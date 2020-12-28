a29e847f30e98e25629d268a1e7f0d5eb78dbb78
spark <ssojeongpark@gmail.com>
2020-12-28 19:39:05 +0800

    Add code

---
+++ Harmonic_approximation/Langevin_Machine_Learning/Integrator/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 11:31:17 2020
+
+@author: simon
+"""
+from .Finite_difference_first_derivative_4particle import Finite_difference_first_derivative
+from .Finite_difference_second_derivative_4particle import Finite_difference_second_derivative
+from .Langevin import Langevin
+from .MCMC import MCMC 
+from .momentum_sampler import momentum_sampler
+from .analytic_method import analytic_method
+
+# =============================================================================
+# Not tested yet 
+# =============================================================================
---
+++ Harmonic_approximation/Langevin_Machine_Learning/Integrator/analytic_method.py
@@ -0,0 +1,86 @@
+import numpy as np
+from tqdm import trange
+from numpy import newaxis
+from .base_simulation import Integration
+
+class analytic_method(Integration):
+
+	def helper(self=None):
+		'''print the common parameters helper'''
+		for parent in analytic_method.__bases__:
+			print(help(parent))
+
+	def __init__(self, *args, **kwargs) -> object:
+
+		super(analytic_method, self).__init__(*args, **kwargs)
+
+		try:
+			self._intSetting = {
+				'iterations': kwargs['iterations'],
+				'DumpFreq': kwargs['DumpFreq'],
+				'time_step': kwargs['time_step'],
+				'integrator_method':kwargs['integrator_method']
+			}
+
+		except:
+			raise TypeError('Integration setting error ( iterations / DumpFreq / gamma / time_step /integrator_method )')
+
+	def integrate(self):
+
+		time_step = self._intSetting['time_step']
+		total_samples = self._intSetting['iterations'] // self._intSetting['DumpFreq']
+		particle = self._configuration['particle']
+		N = self._configuration['N'] # samples
+		DIM = self._configuration['DIM']
+		integrator_method = self._intSetting['integrator_method']
+
+		q_list = np.zeros((total_samples, N, particle,  DIM))
+		p_list = np.zeros((total_samples, N, particle,  DIM))
+
+		for i in trange(total_samples):
+
+
+			q = self._configuration['phase_space'].get_q()
+			p = self._configuration['phase_space'].get_p()
+
+			vecs, y, y_diff = integrator_method(time_step/2,**self._configuration)
+
+			p_temp = np.zeros(y.shape)
+			for z in range(N):
+				p_temp[z] = np.dot(vecs[z], y_diff[z])
+
+			p = p_temp.reshape(-1, particle, DIM)
+
+			self._configuration['phase_space'].set_q(q)
+			self._configuration['phase_space'].set_p(p)
+
+			vecs, y, y_diff = integrator_method(time_step,**self._configuration)
+
+			q_temp = np.zeros(y.shape)
+			for z in range(N):
+				q_temp[z] = np.dot(vecs[z], y[z])
+
+			q = q_temp.reshape(-1,particle,DIM)
+
+			for z in range(N):
+				self._configuration['pb_q'].adjust_real(q[z], self._configuration['BoxSize'])
+
+			self._configuration['phase_space'].set_q(q)
+			self._configuration['phase_space'].set_p(p)
+
+			vecs, y, y_diff = integrator_method(time_step/2,**self._configuration)
+
+			p_temp = np.zeros(y.shape)
+
+			for z in range(N):
+				p_temp[z] = np.dot(vecs[z], y_diff[z])
+
+			p = p_temp.reshape(-1, particle, DIM)
+
+			self._configuration['phase_space'].set_q(q)
+			self._configuration['phase_space'].set_p(p)
+
+			q_list[i] = self._configuration['phase_space'].get_q()
+			p_list[i] = self._configuration['phase_space'].get_p()
+
+		return(q_list, p_list)
