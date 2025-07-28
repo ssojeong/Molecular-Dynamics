14dd86174246b31f20980357b3c3830649e49cd3
ssojeong <62988783+ssojeong@users.noreply.github.com>
2025-07-28 09:45:22 +0800

    Delete HNNwLJ20210825 directory

--- HNNwLJ20210825/data/util_src/run.sh
+++
@@ -1,8 +0,0 @@
-./combineKfiles.sh ../gen_by_MD/f-n4T0.35-hard1-e501 ../gen_by_MD/f-n4T0.35-hard1-e501/crash_info.pt combine2files_crash_wise.py crash
-./combineKfiles.sh ../gen_by_MD/f-n4T0.67-hard1-e501 ../gen_by_MD/f-n4T0.67-hard1-e501/crash_info.pt combine2files_crash_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.35-soft2-e601-ML1 ../gen_by_MC/crash_data2/pw-n4T0.35crashed.pt combine2files_sample_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.35-soft2-e601-ML1 ../gen_by_MC/crash_data2/pw-n4T0.35crashed.pt combine2files_sample_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.67-soft2-e601-ML1 ../gen_by_MC/crash_data2/pw-n4T0.67crashed.pt combine2files_sample_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.35-soft2-e101-ML2 ../gen_by_MC/crash_data2/pw-n4T0.35-soft2-crashed.pt combine2files_sample_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.35-soft2-e101-ML2 ../gen_by_MC/crash_data2/pw-n4T0.35-soft2-crashed.pt combine2files_sample_wise.py crash
-#./combineKfiles.sh ../crash_dir/pw-n4T0.67-soft2-e101-ML2 ../gen_by_MC/crash_data2/pw-n4T0.67-soft2-crashed.pt combine2files_sample_wise.py crash
