e8875478807c9a465d3278d5afb75988369b5b0b
Gobliu <gobliu1215@gmail.com>
2025-09-01 15:42:53 +0800

    rdf

--- code/ML/predicter/plot_rdf.py
+++ code/ML/predicter/plot_rdf.py
@@ -6,15 +6,18 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == '__main__':
+# sys.modules['numpy._core'] = np.core
 
-    hist_list = ['../../../../Data/LLUF/300k_gromacs_histo.pt', '../../../../Data/LLUF/300k_LUFNet_10k_histo.pt']
+if __name__ == '__main__':
+    print(torch.__version__)
+    print(np.__version__)
+    hist_list = ['../../../../Data/LLUF/300k_gromacs_histo.pt', '../../../../Data/LLUF/300k_LLUF_10k_histo.pt']
     name = ['Gromacs', 'LUFNet']
     for f, name in zip(hist_list, name):
-        data = torch.load(f, weights_only=False)
-        counts = data['counts']
-        counts = counts / counts.sum()
-        plt.plot(data['edge_centers'], counts, label=name)
+        data = torch.load(f, weights_only=False, map_location='cpu')
+        gr = data['gr']
+        print(f, gr.shape, data['edge_centers'].shape)
+        plt.plot(data['edge_centers'], gr, label=name)
 
     plt.title('100k steps in Gromacs / 10 k steps in LLUF')
     plt.legend()
